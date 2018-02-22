283e4e9d669d3fb4d47bbf43b24ae149cf0479c5
ShehanPerera <12shehanakalanka@gmail.com>
2018-02-22 16:51:00 +0530

    Added Java docs Added Metrics for Client side

--- src/main/java/com/github/shehanperera/apiagent/MetricServer.java
+++ src/main/java/com/github/shehanperera/apiagent/MetricServer.java
@@ -11,28 +11,33 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
  */
 package com.github.shehanperera.apiagent;
 
 import org.wso2.carbon.config.ConfigurationException;
 import org.wso2.carbon.metrics.core.Histogram;
 import org.wso2.carbon.metrics.core.Level;
+import org.wso2.carbon.metrics.core.Meter;
 import org.wso2.carbon.metrics.core.MetricService;
 import org.wso2.carbon.metrics.core.Metrics;
 import org.wso2.carbon.metrics.core.Timer;
 
+/**
+ * Metrics used for the APIM-3.0
+ */
 public class MetricServer {
-
-    /*
-    * Metrics singleton for use with ballerina gateway
-    * Used Timer and Histogram for request size  
-    *
-    * */
 
     private static final MetricServer metricServer = new MetricServer();
     private MetricService metricService;
-    private Timer responsesTime;
-    private Histogram requestsSize;
+    private Timer backendResponsesTime;
+    private Timer apiResponsesTime;
+    private Meter backendErrorRate;
+    private Meter apiErrorRate;
+    private Histogram backendRequestsSize;
+    private Histogram apiRequestsSize;
+    private Histogram backendResponseSize;
+    private Histogram apiResponseSize;
 
     private MetricServer() {
 
@@ -44,8 +49,22 @@ public class MetricServer {
         }
         metrics.activate();
         metricService = metrics.getMetricService();
-        requestsSize = metricService.histogram(MetricService.name("API Manager", "Request Size"), Level.INFO);
-        responsesTime = metricService.timer(MetricService.name("API Manager", "Time to response"), Level.INFO);
+        backendErrorRate = metricService.meter(MetricService.name("API Manager ",
+                "Backend Error Rate "), Level.INFO);
+        apiErrorRate = metricService.meter(MetricService.name("API Manager ",
+                "API Error Rate "), Level.INFO);
+        backendRequestsSize = metricService.histogram(MetricService.name("API Manager ",
+                "Backend Request Payload Size"), Level.INFO);
+        apiRequestsSize = metricService.histogram(MetricService.name("API Manager ",
+                "API Request Payload Size"), Level.INFO);
+        backendResponseSize = metricService.histogram(MetricService.name("API Manager ",
+                "Backend Response Payload Size"), Level.INFO);
+        apiResponseSize = metricService.histogram(MetricService.name("API Manager ",
+                "API Response Payload Size"), Level.INFO);
+        backendResponsesTime = metricService.timer(MetricService.name("API Manager ",
+                "Backend Calls"), Level.INFO);
+        apiResponsesTime = metricService.timer(MetricService.name("API Manager ",
+                "API Calls"), Level.INFO);
     }
 
     public static MetricServer getInstance() {
@@ -53,14 +72,44 @@ public class MetricServer {
         return metricServer;
     }
 
-    public Histogram getRequestsSize() {
+    public Meter getBackendErrorRate() {
 
-        return requestsSize;
+        return backendErrorRate;
     }
 
-    public Timer getResponsesTime() {
+    public Meter getApiErrorRate() {
 
-        return responsesTime;
+        return apiErrorRate;
+    }
+
+    public Histogram getBackendRequestsSize() {
+
+        return backendRequestsSize;
+    }
+
+    public Histogram getApiRequestsSize() {
+
+        return apiRequestsSize;
+    }
+
+    public Histogram getBackendResponseSize() {
+
+        return backendRequestsSize;
+    }
+
+    public Histogram getApiResponseSize() {
+
+        return apiResponseSize;
+    }
+
+    public Timer getBackendResponsesTime() {
+
+        return backendResponsesTime;
+    }
+
+    public Timer getApiResponsesTime() {
+
+        return apiResponsesTime;
     }
 
 }
